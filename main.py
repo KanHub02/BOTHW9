38cf26ae61c832fc6a286ff8e74b22ec07994c07
Usmanov Kanat <usmnvk200207@gmail.com>
2022-05-03 18:29:25 +0600

    webhook3

--- main.py
+++ main.py
@@ -31,12 +31,12 @@ extra.register_handlers_other(dp)
 
 if __name__ == "__main__":
     # executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
-    executor.set_webhook(
+    executor.start_webhook(
         dispatcher=dp,
         webhook_path='',
         on_startup=on_startup,
         on_shutdown=on_shutdown,
         skip_updates=True,
         host='0.0.0.0',
-        port=int(config('PORT', default=5000))
+        port=int(config("PORT", default=5000))
     )
